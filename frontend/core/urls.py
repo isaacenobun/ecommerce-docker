185ebf62af17b80ec20da6f3cb27940e72a49cbc
isaacenobun <76211324+isaacenobun@users.noreply.github.com>
2025-01-23 20:44:42 +0100

    Updated components

--- frontend/core/urls.py
+++ frontend/core/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('administrator/', views.admin, name='administrator'),
     path('add/', views.add, name='add'),
+    path('edit/<int:id>/', views.edit, name='edit'),
+    path('delete/<int:id>/', views.delete, name='delete'),
     path('customer/', views.customer, name='customer'),
-    path('buy/', views.buy, name='buy'),
+    path('buy/<int:id>', views.buy, name='buy'),
 ]
--- frontend/core/views.py
+++ frontend/core/views.py
@@ -38,7 +38,7 @@ def add(request):
             response.raise_for_status()
             data = response.json()
             
-            messages.success(request, f'The item {request.POST.get("name")}, has been added successsfully')
+            messages.success(request, f'The item {request.POST.get("name")}, has been added successfully')
             
             return redirect('add')
             # return JsonResponse(data, status=response.status_code)
@@ -48,6 +48,54 @@ def add(request):
         
     return render(request, 'add.html')
 
+def edit(request, id):
+    api_url = "http://0.0.0.0:8000/api/v1/item/"+str(id)+"/"
+    
+    if request.method=='POST':
+        
+        payload = {
+            "id": id,
+            "name": request.POST.get('name'),
+            "description": request.POST.get('description'),
+            "stock": request.POST.get('stock')
+        }
+        
+        try:
+            response = requests.put(api_url, json=payload)
+            response.raise_for_status()
+            data = response.json()
+            
+        except requests.exceptions.RequestException as e:
+            return JsonResponse({'error': str(e)}, status=500)
+        
+        messages.success(request, f'Edit has been made successfully')
+        return redirect('administrator')
+    
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+        data = response.json()
+        
+        context = {'data': data}
+        
+        return render(request, 'edit.html', context)
+        
+    except requests.exceptions.RequestException as e:
+            return JsonResponse({'error': str(e)}, status=500)
+        
+def delete(request, id):
+    api_url = "http://0.0.0.0:8000/api/v1/item/"+str(id)+'/'
+    
+    try:
+        response = requests.delete(api_url)
+        response.raise_for_status()
+        messages.success(request, f'Item has been deleted successfully')
+        
+        return redirect('administrator')
+        
+    except requests.exceptions.RequestException as e:
+            return JsonResponse({'error': str(e)}, status=500)
+        
 def customer(request):
     api_url = "http://0.0.0.0:8000/api/v1/item/"
     
@@ -62,10 +110,40 @@ def customer(request):
     except requests.exceptions.RequestException as e:
         return JsonResponse({'error': str(e)}, status=500)
 
-def buy(request):
-    if request.method=='POST':
-        
-        return redirect('customer')
+def buy(request, id):
+    api_url = "http://0.0.0.0:8000/api/v1/item/"+str(id)+"/"
     
-    context = {}
-    return render(request, 'buy.html', context)
+    if request.method=='POST':
+        payload = {
+            "id": id,
+            "stock": request.POST.get('stock')
+        }
+        
+        current_stock = requests.get(api_url)
+        current_stock.raise_for_status()
+        current_stock = int(current_stock.json()['stock'])
+        
+        new_stock = current_stock - int(payload['stock'])
+        payload['stock'] = new_stock
+        
+        try:
+            response = requests.patch(api_url, json=payload)
+            response.raise_for_status()
+            
+            messages.success(request, f'Thank you for your purchase')
+            return redirect('customer')
+        
+        except requests.exceptions.RequestException as e:
+            return JsonResponse({'error': str(e)}, status=500)
+    
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+        data = response.json()
+        
+        context = {'data': data}
+        
+        return render(request, 'buy.html', context)
+    
+    except requests.exceptions.RequestException as e:
+        return JsonResponse({'error': str(e)}, status=500)
